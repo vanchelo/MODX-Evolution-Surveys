0595082022b2f97edb04a15c81942a4405ad641a
vanchelo <npobolka@ya.ru>
2014-10-19 22:57:18 +0300

    Survey model

---
+++ assets/modules/survey/entity/Survey.php
@@ -0,0 +1,35 @@
+<?php
+
+class Survey
+{
+    public $id;
+    public $title = '';
+    public $description = '';
+    public $votes = 0;
+    public $active = 0;
+    public $created_at;
+    public $updated_at;
+    public $closed_at;
+    public $options = array();
+
+    function __construct(array $attributes = array())
+    {
+        if ($attributes) {
+            foreach ($attributes as $name => $value) {
+                if (property_exists($this, $name)) {
+                    $this->{$name} = $value;
+                }
+            }
+        }
+    }
+
+    public function isClosed()
+    {
+        return !empty($this->closed_at);
+    }
+
+    public function isActive()
+    {
+        return (bool) $this->active;
+    }
+}
